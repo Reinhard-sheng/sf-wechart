e6e64c9112ae20e4b242acc252ad9bb15ce00de5
Reinhard Sheng <779081643@qq.com>
2018-02-11 14:12:53 +0800

    fix code about the ident issues

--- adam/models/user.py
+++ adam/models/user.py
@@ -1,7 +1,7 @@
 from oodo import api,fields,models
 
 class AdamUser(models.Model):
-	_name = "adam.user"
+    _name = "adam.user"
 
-	name = fields.Char('姓名')
-	phone = fields.Char('电话')
+    name = fields.Char('姓名')
+    phone = fields.Char('电话')
