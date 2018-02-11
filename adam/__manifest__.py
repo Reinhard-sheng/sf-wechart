f276a509443f073c59068fc36ecce282249f5f51
Reinhard Sheng <779081643@qq.com>
2018-02-11 11:44:12 +0800

    new module adam

---
+++ adam/__manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# Copyright <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Adam",
+    "summary": "This module is created for test by Reinhard",
+    "version": "11.0.1.0.0",
+    "category": "",
+    "website": "",
+    "author": "Reinhard",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    # "pre_init_hook": "pre_init_hook",
+    # "post_init_hook": "post_init_hook",
+    # "post_load": "post_load",
+    # "uninstall_hook": "uninstall_hook",
+    # "external_dependencies": {
+    #     "python": [],
+    #     "bin": [],
+    # },
+    "depends": [
+        "base",
+    ],
+    "data": [
+        # "security/some_model_security.xml",
+        # "security/ir.model.access.csv",
+        # "templates/assets.xml",
+        # "views/report_name.xml",
+        # "views/res_partner_view.xml",
+        # "wizards/wizard_model_view.xml",
+    ],
+    "demo": [
+        # "demo/res_partner_demo.xml",
+    ],
+    "qweb": [
+        # "static/src/xml/module_name.xml",
+    ]
+}
---
+++ adam/models/user.py
@@ -0,0 +1,7 @@
+from oodo import api,fields,models
+
+class AdamUser(models.Model):
+	_name = "adam.user"
+
+	name = fields.Char('姓名')
+	phone = fields.Char('电话')
