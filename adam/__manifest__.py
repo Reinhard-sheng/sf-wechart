470c0f016591ea79227c290b5200b2d9e13c61ca
Reinhard Sheng <779081643@qq.com>
2018-02-11 14:20:39 +0800

    set the utf-8 head,cause i don't know exactlly is the python2.7 or python3

--- adam/__manifest__.py
+++ adam/__manifest__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright <YEAR(S)> <AUTHOR(S)>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
 {
     "name": "Adam",
     "summary": "This module is created for test by Reinhard",
