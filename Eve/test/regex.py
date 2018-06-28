64554a70a883bb3a57e78af8ab647ec2cddf6be8
Reinhard <779081643@qq.com>
2018-06-28 23:39:26 +0800

    remove files

--- Eve/test/regex.py
+++
@@ -1,5 +0,0 @@
-import re
-
-regex_private = re.compile(r'^(_.*|init)$')
-
-print regex_private.match('init').group()
--- adam/models/user.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from oodo import api,fields,models
-
-class AdamUser(models.Model):
-    _name = "adam.user"
-
-    name = fields.Char('姓名')
-    phone = fields.Char('电话')
